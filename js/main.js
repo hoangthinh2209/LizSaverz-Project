284e44703d71e69b5e3ff3fa6967ff2f6c33d289
hoangthinh <thinhhoang2209@gmail.com>
2022-11-02 21:10:27 +0700

    update login ok

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function getEle(id) {
+  return document.getElementById(id);
+}
+function checkoutlogin() {
+  var checkoutPass = getEle("loginPassword");
+  var checkoutName = getEle("loginName");
+  var thongbao = getEle("thongbao");
+  var alert = getEle("alert");
+  if (
+    checkoutName.value == "Hoangthinh2209" &&
+    checkoutPass.value == "dinhthinh"
+  ) {
+    alert.style.backgroundColor = "green";
+    thongbao.innerHTML = "Đăng nhập thành công";
+  } else {
+    alert.style.backgroundColor = "red";
+    thongbao.innerHTML = "Đăng nhập không thành công";
+  }
+}
+
+onClick();
